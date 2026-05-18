581a8de61adef6efc4055880b53e0bdfe1d0e2a7
Ortega <allanmashanda@gmail.com>
2026-05-19 00:55:30 +0300

    Populate firebase config

---
+++ firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "REDACTED_ROTATE_THIS_KEY",
+  authDomain: "vvdtcoastalzone.firebaseapp.com",
+  projectId: "vvdtcoastalzone",
+  storageBucket: "vvdtcoastalzone.firebasestorage.app",
+  messagingSenderId: "260883478600",
+  appId: "1:260883478600:web:470c36306340300f63807d"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+window.db = db;
+
+console.log("Firebase + Firestore connected");
